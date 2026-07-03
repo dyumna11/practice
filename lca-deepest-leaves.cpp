83e1745bfab11b794e109a4630b98e46fb403a80
Dyumna <dyumna.negi1@gmail.com>
2026-07-04 00:54:37 +0530

    Create lca-deepest-leaves.cpp

---
+++ lca-deepest-leaves.cpp
@@ -0,0 +1,17 @@
+
+pair<TreeNode*,int>dfs(TreeNode* root)
+{
+    if(!root)
+    return {nullptr,0};
+    auto left=dfs(root->left);
+    auto right=dfs(root->right);
+    if(left.second==right.second)
+    return {root,left.second+1};
+    else if(left.second<right.second)
+     return {right.first,right.second+1};
+     else
+      return {left.first,left.second+1};
+}
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+       return dfs(root).first;
+    }
